b9408b853918f17a741ec4eb006c4a25569fed0c
cristiii <cristian.vlad10@gmail.com>
2023-03-04 11:55:42 +0200

    volatile and atomic operations

---
+++ src/main/java/org/example/AtomicAndVolatile.java
@@ -0,0 +1,80 @@
+package org.example;
+
+import java.util.Random;
+
+public class AtomicAndVolatile {
+
+    public static void main(String[] args) {
+        Metrics metrics = new Metrics();
+
+        BusinessLogic businessLogicThread1 = new BusinessLogic(metrics);
+        BusinessLogic businessLogicThread2 = new BusinessLogic(metrics);
+
+        MetricsPrinter printer = new MetricsPrinter(metrics);
+
+        businessLogicThread1.start();
+        businessLogicThread2.start();
+        printer.start();
+    }
+
+    private static class MetricsPrinter extends Thread {
+        private Metrics metrics;
+
+        public MetricsPrinter(Metrics metrics) {
+            this.metrics = metrics;
+        }
+
+        @Override
+        public void run() {
+            while(true) {
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+
+                }
+                double currentAverage = metrics.getAverage();
+                System.out.println("Current average is: " + currentAverage);
+            }
+        }
+    }
+
+    private static class BusinessLogic extends Thread {
+        private Metrics metrics;
+        private Random random = new Random();
+
+        public BusinessLogic(Metrics metrics) {
+            this.metrics = metrics;
+        }
+
+        @Override
+        public void run() {
+            while (true) {
+                long start = System.currentTimeMillis();
+                try {
+                    Thread.sleep(random.nextInt(10));
+                } catch (InterruptedException e) {
+
+                }
+                long end = System.currentTimeMillis();
+                metrics.addSample(end - start);
+            }
+        }
+    }
+
+    private static class Metrics {
+        private long count = 0;
+
+        // volatile will guarantee that also the read from getAverage() is atomic
+        private volatile double average = 0.0;
+
+        public synchronized void addSample(long sample) {
+            double currentSum = average * count;
+            count++;
+            average = (currentSum + sample) / count;
+        }
+
+        public double getAverage() {
+            return average;
+        }
+    }
+}
